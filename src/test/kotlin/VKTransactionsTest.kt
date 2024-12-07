f0d1221cb1fff658c3cf5af7815ef104e7d895b3
Stiller <schatirow@mail.ru>
2024-12-07 22:02:21 +0300

    Test should fail

--- src/test/kotlin/VKTransactionsTest.kt
+++ src/test/kotlin/VKTransactionsTest.kt
@@ -53,7 +53,7 @@ class VKTransactionsTest {
         val card = "VK Pay"
         val monthSum = 0
         val transaction = 20_000
-        val expected = VKTransactions.ERR_DAY_LIMIT
+        val expected = 0//VKTransactions.ERR_DAY_LIMIT
         val result = VKTransactions.calculateCommission(card, monthSum, transaction)
         assertEquals(expected, result)
     }
