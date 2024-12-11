af2d0ba27c5b273c707e9f11b8dfbc3ab7318284
Stiller <schatirow@mail.ru>
2024-12-11 21:03:45 +0300

    Added positive tests

--- src/test/kotlin/VKTransactionsTest.kt
+++ src/test/kotlin/VKTransactionsTest.kt
@@ -53,7 +53,7 @@ class VKTransactionsTest {
         val card = "VK Pay"
         val monthSum = 0
         val transaction = 20_000
-        val expected = 0//VKTransactions.ERR_DAY_LIMIT
+        val expected = VKTransactions.ERR_DAY_LIMIT
         val result = VKTransactions.calculateCommission(card, monthSum, transaction)
         assertEquals(expected, result)
     }
@@ -63,6 +63,33 @@ class VKTransactionsTest {
         val monthSum = 35_000
         val transaction = 10_000
         val expected = VKTransactions.ERR_MONTH_LIMIT
+        val result = VKTransactions.calculateCommission(card, monthSum, transaction)
+        assertEquals(expected, result)
+    }
+    @Test
+    fun visaMirSmallSum() {
+        val card = "Visa"
+        val monthSum = 0
+        val transaction = 100
+        val expected = 35
+        val result = VKTransactions.calculateCommission(card, monthSum, transaction)
+        assertEquals(expected, result)
+    }
+    @Test
+    fun maestroMasterCardSmallSum() {
+        val card = "Maestro"
+        val monthSum = 0
+        val transaction = 400
+        val expected = 0
+        val result = VKTransactions.calculateCommission(card, monthSum, transaction)
+        assertEquals(expected, result)
+    }
+    @Test
+    fun maestroMasterCardBigSum() {
+        val card = "Maestro"
+        val monthSum = 75_000
+        val transaction = 400
+        val expected = 22
         val result = VKTransactions.calculateCommission(card, monthSum, transaction)
         assertEquals(expected, result)
     }
